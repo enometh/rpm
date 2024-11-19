64408b9b750d37d27252b093b59541ca41aa262f
Madhu <enometh@net.meer>
2024-11-19 09:38:08 -0700

    rpmio/lposix.hh: fix external C linkage

--- rpmio/lposix.hh
+++ rpmio/lposix.hh
@@ -3,10 +3,19 @@
 
 #include <rpm/rpmutil.h>
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 RPM_GNUC_INTERNAL
 void check_deprecated(lua_State *L, const char *func, const char *deprecated_in);
 
 RPM_GNUC_INTERNAL
 int luaopen_posix (lua_State *L);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
